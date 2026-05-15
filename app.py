b8c84ecbc6cae3752c700ff330ab0c818538a0b0
joaorocha-bit <joao.rocha@hmv.org.br>
2026-05-15 13:55:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -394,7 +394,7 @@ renderizar();
 </body>
 </html>"""
 
-    components.html(html, height=800, scrolling=False)
+    components.html(html, height=700, scrolling=False)
 
 else:
     st.error("Erro ao carregar dados.")
